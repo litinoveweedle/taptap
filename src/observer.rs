90f35fb841b71447548e1a3296c87334568c1268
litinoveweedle <15144712+litinoveweedle@users.noreply.github.com>
2025-10-25 14:11:34 +0200

    added infrastructure even in observe mode - to output in JSON gateway and node details (addresses, barcodes, versions etc.)

--- src/observer.rs
+++ src/observer.rs
@@ -28,8 +28,11 @@ use std::time::SystemTime;
 
 pub mod event;
 
+mod persistent_state;
+use persistent_state::{PersistentState, PersistentStateEvent};
+
 mod node_table;
-use node_table::{NodeTable, NodeTableBuilder};
+use node_table::NodeTableBuilder;
 
 mod slot_clock;
 use slot_clock::SlotClock;
@@ -65,28 +68,6 @@ impl Observer {
     pub fn persistent_state(&self) -> &PersistentState {
         &self.persistent_state
     }
-
-    fn gateway(&self, id: GatewayID) -> event::Gateway {
-        let address = self.persistent_state.gateway_identities.get(&id).copied();
-        event::Gateway { id, address }
-    }
-
-    fn node(&self, gateway_id: GatewayID, id: NodeID) -> event::Node {
-        let address = self
-            .persistent_state
-            .gateway_node_tables
-            .get(&gateway_id)
-            .and_then(|node_table| node_table.0.get(&id))
-            .copied();
-
-        let barcode = address.map(|addr| addr.into());
-
-        event::Node {
-            id,
-            address,
-            barcode,
-        }
-    }
 }
 
 impl gateway::transport::Sink for Observer {
@@ -108,6 +89,8 @@ impl gateway::transport::Sink for Observer {
             self.persistent_state
                 .gateway_identities
                 .insert(gateway_id, address);
+            let infrastructure_event = PersistentStateEvent::from(&self.persistent_state);
+            println!("{}", serde_json::to_string(&infrastructure_event).unwrap());
         }
     }
 
@@ -122,6 +105,8 @@ impl gateway::transport::Sink for Observer {
             self.persistent_state
                 .gateway_versions
                 .insert(gateway_id, version);
+            let infrastructure_event = PersistentStateEvent::from(&self.persistent_state);
+            println!("{}", serde_json::to_string(&infrastructure_event).unwrap());
         }
     }
 
@@ -133,6 +118,8 @@ impl gateway::transport::Sink for Observer {
             // existing state
             self.persistent_state.gateway_identities = enumeration_state.gateway_identities;
             self.persistent_state.gateway_versions = enumeration_state.gateway_versions;
+            let infrastructure_event = PersistentStateEvent::from(&self.persistent_state);
+            println!("{}", serde_json::to_string(&infrastructure_event).unwrap());
         }
     }
 
@@ -161,24 +148,36 @@ impl gateway::transport::Sink for Observer {
     fn packet_received(
         &mut self,
         _gateway_id: GatewayID,
-        _header: &ReceivedPacketHeader,
-        _data: &[u8],
+        _packet_header: &ReceivedPacketHeader,
+        _packet_data: &[u8],
     ) {
     }
 
     fn command_executed(
         &mut self,
         _gateway_id: GatewayID,
-        _request: (PacketType, &[u8]),
-        _response: (PacketType, &[u8]),
+        _command_request: (PacketType, &[u8]),
+        _command_response: (PacketType, &[u8]),
     ) {
     }
 }
 
 impl pv::application::Sink for Observer {
-    fn string_request(&mut self, _gateway_id: GatewayID, _pv_node_id: NodeID, _request: &str) {}
+    fn string_request(
+        &mut self,
+        _gateway_id: GatewayID,
+        _pv_node_id: NodeID,
+        _string_request: &str,
+    ) {
+    }
 
-    fn string_response(&mut self, _gateway_id: GatewayID, _pv_node_id: NodeID, _response: &str) {}
+    fn string_response(
+        &mut self,
+        _gateway_id: GatewayID,
+        _pv_node_id: NodeID,
+        _string_response: &str,
+    ) {
+    }
 
     fn node_table_page(
         &mut self,
@@ -192,6 +191,8 @@ impl pv::application::Sink for Observer {
             self.persistent_state
                 .gateway_node_tables
                 .insert(gateway_id, new_table);
+            let infrastructure_event = PersistentStateEvent::from(&self.persistent_state);
+            println!("{}", serde_json::to_string(&infrastructure_event).unwrap());
         }
     }
 
@@ -206,7 +207,7 @@ impl pv::application::Sink for Observer {
     fn power_report(
         &mut self,
         gateway_id: GatewayID,
-        pv_node_id: NodeID,
+        node_id: NodeID,
         power_report: &pv::application::PowerReport,
     ) {
         let Some(slot_clock) = self.slot_clocks.get(&gateway_id) else {
@@ -218,12 +219,8 @@ impl pv::application::Sink for Observer {
             return;
         };
 
-        let Ok(event) = event::PowerReportEvent::new(
-            self.gateway(gateway_id),
-            self.node(gateway_id, pv_node_id),
-            slot_clock,
-            power_report,
-        ) else {
+        let Ok(event) = event::PowerReportEvent::new(gateway_id, node_id, slot_clock, power_report)
+        else {
             log::error!(
                 "discarding power report from gateway {:?} due to invalid slot counter: {:?}",
                 gateway_id,
@@ -234,18 +231,6 @@ impl pv::application::Sink for Observer {
 
         println!("{}", serde_json::to_string(&event).unwrap());
     }
-}
-
-/// Persistent state of an observed network.
-///
-/// Information like hardware addresses and version numbers are exchanged infrequently. This data
-/// is captured and stored in `PersistentState`.
-#[derive(Debug, Clone, Eq, PartialEq, Default, serde::Serialize, serde::Deserialize)]
-pub struct PersistentState {
-    gateway_node_tables: BTreeMap<GatewayID, NodeTable>,
-
-    gateway_identities: BTreeMap<GatewayID, LongAddress>,
-    gateway_versions: BTreeMap<GatewayID, String>,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, serde::Serialize, serde::Deserialize)]
